136f07661b503b5f5904b0c8ad68634feacd7756
TheCrabilia <ilja.spirts@outlook.com>
2020-10-28 15:59:53 +0200

    Init commit

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ ! $(command -v sudo) ]; then
+	echo "Sudo not found! Please install Sudo."
+	exit 1
+fi
+
+if [ $(command -v apt) ]; then
+	echo "Apt manager found!"
+	MANAGER="apt"
+fi
+
+if [ $(command -v yum) ]; then
+	echo "Yum manager found!"
+	MANAGER="yum"
+fi
+
+sudo $MANAGER update -y
+sudo $MANAGER install -y zsh chroma
+
+cp ./zshrc $HOME/.zshrc
+cp -r ./oh-my-zsh $HOME/.oh-my-zsh
+source $HOME/.zshrc
