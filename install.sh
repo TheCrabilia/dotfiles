f2764b05aa8c559cee298cea1ea2bf3b7d1921c2
TheCrabilia <ilja.spirts@outlook.com>
2020-10-28 17:40:32 +0200

    Init commit

--- install.sh
+++ install.sh
@@ -23,6 +23,7 @@ fi
 cp ./zshrc $HOME/.zshrc
 cp -r ./oh-my-zsh $HOME/.oh-my-zsh
 
-chsh -s $(which zsh)
+#chsh -s $(which zsh)
+usermod --shell $(which zsh) $USER
 # source $HOME/.zshrc
 echo "All done! Please restart your terminal!"
