8299af7f311b7d7be97b7e0e402640860a2a3bfa
TheCrabilia <ilja.spirts@outlook.com>
2020-10-28 17:34:36 +0200

    Init commit

--- install.sh
+++ install.sh
@@ -7,19 +7,22 @@ fi
 
 if [ $(command -v apt) ]; then
 	echo "Apt manager found!"
-	MANAGER="apt"
+	sudo apt update
+	sudo apt install -y zsh chroma
 fi
 
 if [ $(command -v yum) ]; then
 	echo "Yum manager found!"
-	MANAGER="yum"
+	sudo yum update -y
+	sudo yum install -y zsh libchromaprint-devel
 fi
 
-sudo $MANAGER update -y
-sudo $MANAGER install -y zsh chroma
+# sudo $MANAGER update -y
+# sudo $MANAGER install -y zsh chroma
 
 cp ./zshrc $HOME/.zshrc
 cp -r ./oh-my-zsh $HOME/.oh-my-zsh
 
 chsh -s $(which zsh)
-source $HOME/.zshrc
+# source $HOME/.zshrc
+echo "All done! Please restart your terminal!"
