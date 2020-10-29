f642f6a45a442ce79313217fabc68dfed72d5f9c
TheCrabilia <ilja.spirts@outlook.com>
2020-10-29 21:26:54 +0200

    Make improvements

--- install.sh
+++ install.sh
@@ -9,18 +9,20 @@ if [ ! $(command -v sudo) ]; then
 	exit 1
 fi
 
+# Debian, etc.
 if [ $(command -v apt) ]; then
 	echo "Apt manager found!"
 	echo "Installing required staff..."
 	sudo apt update >/dev/null
-	sudo apt install -y curl git zsh chroma >/dev/null
+	sudo apt install -q -y curl git zsh chroma >/dev/null
 fi
 
+# RHEL, etc.
 if [ $(command -v yum) ]; then
 	echo "Yum manager found!"
 	echo "Installing required staff..."
-	sudo yum update -y >/dev/null
-	sudo yum install -y curl git zsh libchromaprint-devel >/dev/null
+	sudo yum update -yq
+	sudo yum install -yq curl git zsh libchromaprint-devel
 fi
 
 echo "Installing Oh-My-Zsh framework..."
@@ -34,13 +36,13 @@ sudo usermod --shell $(which zsh) $USER
 
 echo "Installing ZSH external plugins..."
 # zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-echo "Zsh-AutoSuggestions plugin (${GREEN}Done${NC})"
+git clone -q https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo "Zsh-AutoSuggestions plugin (Done)"
 # zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-echo "Zsh-Syntax-Highlighting plugin (${GREEN}Done${NC}"
+git clone -q https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "Zsh-Syntax-Highlighting plugin (Done)"
 # zsh-256color
-git clone https://github.com/chrissicool/zsh-256color.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-256color
-echo "Zsh-256color plugin (${GREEN}Done${NC})"
+git clone -q https://github.com/chrissicool/zsh-256color.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-256color
+echo "Zsh-256color plugin (Done)"
 
 echo "All done! Please restart your terminal!"
