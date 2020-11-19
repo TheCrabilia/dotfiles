bb74201f975e6f3f08e3af646e47ab49e681dd0d
TheCrabilia <ilja.spirts@outlook.com>
2020-11-19 16:58:54 +0200

    Change lscolors

--- zsh/preset.zsh
+++ zsh/preset.zsh
@@ -1,8 +1,8 @@
 HIST_STAMPS="dd.mm.yyyy"
 
 # Colors for ls
-export LSCOLORS="exgxcxcxbxegedabagacad"
-export LS_COLORS="di=34:ln=36:so=32:pi=32:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
+export LSCOLORS="gxFxdxcxCxegedabagacad"
+export LS_COLORS="di=36:ln=1;35:so=33:pi=32:ex=1;32:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
 export CLICOLOR=1
 
 # Fix broken terminals
