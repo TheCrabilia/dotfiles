d11e8802215f8e243e1459b0a94e121d8ea11660
TheCrabilia <ilja.spirts@outlook.com>
2020-12-06 19:57:30 +0200

    Add vim plugin auto installation, correct mistakes

--- install.sh
+++ install.sh
@@ -1,39 +1,38 @@
 #!/usr/bin/env bash
 
-if ! which sudo >/dev/null; then
+if ! which sudo &>/dev/null; then
 	echo "Sudo not found! Please install Sudo."
 	exit 1
 fi
 
 # Debian, etc.
-if which apt >/dev/null; then
+if which apt &>/dev/null; then
 	echo "Apt manager found! Installing required staff..."
 	sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq zsh python3-pygments vim curl wget < /dev/null > /dev/null
 fi
 
 # RHEL, etc.
-if which yum >/dev/null; then
+if which yum &>/dev/null; then
 	echo "Yum manager found! Installing required staff..."
 	MANAGER=yum
 	sudo $MANAGER install -qq epel-release zsh python3-pygments vim curl wget
 fi
 
-# Install vim-plug
+echo "Installing vim plugin manager..."
 curl -sfLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# Copy required files to user home directory
+echo "Installing vim plugins..."
+if which vim &>/dev/null; then
+	VIM=vim
+elif which nvim &>/dev/null; then
+	VIM=nvim
+fi
+
+$VIM -c PlugInstall! -c qa!
+
+echo "Copying requered files"
 cp ./zshrc $HOME/.zshrc
 cp ./zshenv $HOME/.zshenv
 cp ./vimrc $HOME/.vimrc
 
-# Change default user shell if it is not zsh
-if [[ $SHELL != /usr/bin/zsh ]]; then
-	echo "Changing user default shell to ZSH..."
-	if which chsh >/dev/null; then
-		chsh -s /bin/zsh $USER
-	else
-		sudo usermod -s /bin/zsh $USER
-	fi
-fi
-
-echo "Done! Now you can run zsh!"
+echo "Done!"
