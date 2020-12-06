678ec8f3735e514cb49c77f614678b3394b73385
TheCrabilia <ilja.spirts@outlook.com>
2020-12-06 20:00:51 +0200

    Change zinit plugin manager to zplug plugin manager

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,49 +1,58 @@
-## Load plugins
-# Load zinit if available
-local zinit=~/.zinit/bin/zinit.zsh
-if [[ -f $zinit ]]; then
-	omz_plugins=(
+local zplug_home=$HOME/.zplug
+if [[ -d $zplug_home ]]; then
+	oh_my_zsh_plugins=(
 		git
-		#docker
+		docker
 		docker-compose
 		colorize
 	)
 
-	zsh_plugins=(
+	plugins=(
 		chrissicool/zsh-256color
 		zsh-users/zsh-autosuggestions
 		zsh-users/zsh-history-substring-search
 		zsh-users/zsh-syntax-highlighting
 	)
 
-	source $zinit
-	# Install Oh-My-Zsh plugins
-	foreach plugin in $omz_plugins
-		zinit snippet OMZP::$plugin
+	source $zplug_home/init.zsh
+	foreach plugin in $oh_my_zsh_plugins
+		zplug "plugins/$plugin", from:oh-my-zsh
 	end
-	# Install custom plugins
-	foreach plugin in $zsh_plugins
-		zinit light $plugin
+	foreach plugin in $plugins
+		zplug "$plugin"
 	end
+
+	# Install plugins if there are plugins that are not been installed
+	if ! zplug check --verbose; then
+		printf "Install? [y/N]: "
+		if read -q; then
+			echo; zplug install
+		fi
+	fi
+
+	# Load zplug
+	zplug load
+
+	# Bind up/down to substring search if zsh-substring-search installed
+	if [[ -d $ZPLUG_REPOS/zsh-users/zsh-history-substring-search ]]; then
+		zle -N history-substring-search-up
+		zle -N history-substring-search-down
+		bindkey '^[[A' history-substring-search-up
+		bindkey '^[[B' history-substring-search-down
+	fi
+else
+	# Install zplug if it is not already installed
+	git clone -q https://github.com/zplug/zplug $zplug_home
 fi
-
-## Install plugin installers and some other staff :)
-function install_zinit {
-	mkdir ~/.zinit
-	git clone https://github.com/zdharma/zinit.git ~/.zinit/bin
-	source $HOME/.zshrc
-}
-
-function update_zsh_plugins {
-	zinit update --all
-}
 
 ## Plugins settings
 # ZSH-AUTOSUGGEST
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=8"
 ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
-ZSH_AUTOSUGGEST_HISTORY_IGNORE="git *"
+ZSH_AUTOSUGGEST_HISTORY_IGNORE=("git *| rsync *| scp *")
+ZSH_AUTOSUGGEST_COMPLETION_IGNORE=("git *| rsync *| scp *")
+ZSH_AUTOSUGGEST_USE_ASYNC="yes"
 
 # ZSH-SYNTAX-HIGHLIGHT
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
