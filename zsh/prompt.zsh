eec050deb39925177b765a5172bd56432f137c12
TheCrabilia <ilja.spirts@outlook.com>
2020-12-06 19:59:18 +0200

    Change prompt style

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,30 +1,17 @@
 # Allow variable substitution inside prompts
 setopt prompt_subst
 
-# Draw different symbols depending on terminal
-if [ $TERM = xterm-256color ]; then
-	prompt_hello="➤"
-	git_symbol_dirty=" ✗"
-        git_symbol_clean=" ✔"
-else
-	prompt_hello=">"
-	git_symbol_dirty=" -"
-        git_symbol_clean=" +"
-fi
-
-# Draw user@hostname if it is ssh connection
-if [[ -c $SSH_TTY ]]; then
-	ssh_conn='%F{yellow}%n@%m%f '
-fi
+# Draw hostname if it is ssh connection
+[[ -c $SSH_TTY ]] && hn='%m '
 
 # Red prompt if root
-local user_color="%F{red}%F{green}"; [ $UID -eq 0 ] && user_color="%F{red}"
+local hn_color='%F{green}'; [ $UID -eq 0 ] && hn_color=''
 
-ZSH_THEME_GIT_PROMPT_PREFIX="%F{blue}(%f"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%F{blue})%f "
-ZSH_THEME_GIT_PROMPT_CLEAN="%F{red}%F{green}$git_symbol_clean%f"
-ZSH_THEME_GIT_PROMPT_DIRTY="%F{red}$git_symbol_dirty%f"
+ZSH_THEME_GIT_PROMPT_PREFIX=' | '
+#ZSH_THEME_GIT_PROMPT_SUFFIX=''
+ZSH_THEME_GIT_PROMPT_CLEAN=' %F{red}%F{green}+%f'
+ZSH_THEME_GIT_PROMPT_DIRTY=' %F{red}-%f'
 
 # Prompt variables
-PROMPT='$ssh_conn$(git_prompt_info)%f$user_color$prompt_hello%f '
-RPROMPT='%~ %F{red}%?%f'
+PROMPT='%F{red}[$hn_color$hn%f%~%F{red}%f$(git_prompt_info)%F{blue}%F{red}]#%f '
+
