1adc12bc836505e6f062be46ea95f04670239159
TheCrabilia <ilja.spirts@outlook.com>
2020-12-12 19:24:54 +0200

    Change prompt hello symbol

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -5,7 +5,7 @@ setopt prompt_subst
 [[ -c $SSH_TTY ]] || [[ $UID -eq 0 ]]  && hn='%m '
 
 # Red prompt if root
-local user_color='%F{green}'; [ $UID -eq 0 ] && user_color='%F{red}'
+local user_prompt='%F{green}$%f'; [ $UID -eq 0 ] && user_prompt='%F{red}#%f'
 
 ZSH_THEME_GIT_PROMPT_PREFIX=' | '
 #ZSH_THEME_GIT_PROMPT_SUFFIX=''
@@ -13,5 +13,5 @@ ZSH_THEME_GIT_PROMPT_CLEAN=' %F{red}%F{green}+%f'
 ZSH_THEME_GIT_PROMPT_DIRTY=' %F{red}-%f'
 
 # Prompt variables
-PROMPT='%F{red}[%F{green}$hn%f%~%F{red}%f$(git_prompt_info)%F{blue}%F{red}]%f$user_color#%f '
+PROMPT='%F{red}[%F{green}$hn%f%~%F{red}%f$(git_prompt_info)%F{blue}%F{red}]%f$user_prompt '
 
