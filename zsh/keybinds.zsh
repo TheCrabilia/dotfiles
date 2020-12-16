7cb9c0d07d68449c101920298232e0d2b534c45d
TheCrabilia <ilja.spirts@outlook.com>
2020-12-16 13:10:34 +0200

    Add new keybinds

--- zsh/keybinds.zsh
+++ zsh/keybinds.zsh
@@ -8,3 +8,13 @@ if [[ -d $ZPLUG_REPOS/zsh-users/zsh-history-substring-search ]]; then
 	bindkey '^[[A' history-substring-search-up
 	bindkey '^[[B' history-substring-search-down
 fi
+
+bindkey '\eOH' beginning-of-line	# Home
+bindkey '\e[H' beginning-of-line	# Home (xorg)
+bindkey '\e[1~' beginning-of-line	# Home (console)
+bindkey '\eOF' end-of-line			# End
+bindkey '\e[F' end-of-line			# End (xorg)
+bindkey '\e[4~' end-of-line			# End (console)
+bindkey '\e[2~' overwrite-mode		# Ins
+bindkey '\e[3~' delete-char			# Del
+
