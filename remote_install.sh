6a3fe63f74df5849a3a79aa34d4bedb52e14466a
TheCrabilia <69972734+TheCrabilia@users.noreply.github.com>
2021-08-25 17:53:03 +0300

    Created remote install script

---
+++ remote_install.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+mtenv_path=$HOME/.mtenv
+distro=$(cat /etc/os-release | grep -E "^ID=" | sed -e 's/^ID=//' -e 's/"//g')
+
+case $distro in
+    arch)
+        echo "Arch like Linux distribution found. Installing required packages..."
+        pacman -S zsh vim curl wget git gawk
+        ;;
+
+    debian|ubuntu)
+        echo "Debian like Linux distribution found. Installing required packages..."
+        apt-get install -y zsh vim curl wget git gawk
+        ;;
+
+    almalinux|centos|fedora)
+        echo "RHEL like Linux distribution found. Installing required packages..."
+        dnf install -y epel-release zsh vim curl wget git gawk
+        ;;
+
+    *)
+        echo "No supported Linux distribution found. Exiting..."
+        exit 1
+        ;;
+esac
+
+echo "Cloning mtenv repository to home directory..."
+git clone https://github.com/TheCrabilia/mtenv $mtenv_path
+
+echo "Installing vim plugin manager..."
+curl -sfLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "Installing vim plugins..."
+vim -c PlugInstall! -c qa!
+
+echo "Copying terminal environment files..."
+cd $mtenv_path
+cfg_files=( zshrc zshenv vimrc tmux.conf )
+for file in $cfg_files; do
+    cp $file $HOME/.$file
+done
+cp zshrc $HOME/.zshrc
+cp zshenv $HOME/.zshenv
+cp vimrc $HOME/.vimrc
+cp tmux $HOME/.tmux.conf
+
+echo "Done."
