164844475c4bf561d47e1dd85fe65ee6814ae1c3
TheCrabilia <69972734+TheCrabilia@users.noreply.github.com>
2022-05-15 18:22:12 +0300

    Fix completion

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,31 +1,18 @@
+# Add custom completion directory to fpath
+fpath=(/opt/homebrew/share/zsh/site-functions $HOME/.zsh/completion $fpath)
+
 # Enable completion features
 autoload -Uz compinit && compinit
 autoload bashcompinit && bashcompinit
 zstyle ':complition:*:*:*:*:*' menu select
 zstyle ':complition:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'   # case insensitive tab complition
 zstyle ':complition:*' list-colors "${(s.:.)LS_COLORS}" 	# Take advantage of $LS_COLORS for completion as well
-
-# Add custom completion directory to fpath
-fpath=($HOME/.zsh/completion $fpath)
-
-# kubectl completion
-if command -v kubectl >/dev/null; then
-    eval "$(kubectl completion zsh)"
-fi
+zstyle ':completion:*:*:docker:*' option-stacking yes
+zstyle ':completion:*:*:docker-*:*' option-stacking yes
 
 # kubectl certificate manager completion
 if command -v cmctl >/dev/null; then
-    cmctl completion zsh > $fpath[1]/_cmctl
-fi
-
-# aws cli completion
-if command -v aws >/dev/null; then
-    complete -C $(which aws_completer) aws
-fi
-
-# helm completion
-if command -v helm >/dev/null; then
-    eval "$(helm completion zsh)"
+    cmctl completion zsh > $fpath[2]/_cmctl
 fi
 
 # minikube completion
@@ -33,12 +20,13 @@ if command -v minikube >/dev/null; then
     eval "$(minikube completion zsh)"
 fi
 
-# tekton-cli completion
-if command -v tkn >/dev/null; then
-    eval "$(tkn completion zsh)"
-fi
-
 # terraform completion
 if command -v terraform >/dev/null; then
     complete -o nospace -C $(which terraform) terraform
+fi
+
+# 1passowrd-cli completion
+if command -v op >/dev/null; then
+    eval "$(op completion zsh)"
+    compdef _op op
 fi
