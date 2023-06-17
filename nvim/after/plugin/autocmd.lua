478e2f2bea7ab60191c2dc64a593b1337fa129c2
TheCrabilia <69972734+TheCrabilia@users.noreply.github.com>
2023-06-17 13:36:45 +0300

    Add yank highlight autocmd

--- nvim/after/plugin/autocmd.lua
+++ nvim/after/plugin/autocmd.lua
@@ -1,11 +1,27 @@
+local cursorline_group = vim.api.nvim_create_augroup("cursorline", {})
+
 vim.api.nvim_create_autocmd("WinLeave", {
+	group = cursorline_group,
 	callback = function()
 		vim.wo.cursorline = false
 	end,
 })
 
 vim.api.nvim_create_autocmd("WinEnter", {
+	group = cursorline_group,
 	callback = function()
 		vim.wo.cursorline = true
+	end,
+})
+
+-- Hightlight yanked text
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("yank_highlight", {}),
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 40,
+		})
 	end,
 })
