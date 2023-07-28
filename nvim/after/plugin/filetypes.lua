75e22dbf1b0e7debc6b19f25aa22fa80c4d8a74f
TheCrabilia <69972734+TheCrabilia@users.noreply.github.com>
2023-07-28 14:32:19 +0300

    feat: add Helm template file support

--- nvim/after/plugin/filetypes.lua
+++ nvim/after/plugin/filetypes.lua
@@ -18,5 +18,24 @@ vim.filetype.add({
 				vim.bo.shiftwidth = 3
 			end
 		end,
+		yaml = function()
+         local ft = ""
+			local dirname = vim.fs.dirname(vim.api.nvim_buf_get_name(0))
+			if
+				vim.fs.find("Chart.yaml", {
+					upward = true,
+					stop = vim.loop.os_homedir(),
+					path = dirname,
+				}) and vim.regex(".*/templates.*"):match_str(dirname)
+			then
+            ft = "helm"
+         else
+            ft = "yaml"
+			end
+         return ft, function()
+            vim.bo.tabstop = 2
+            vim.bo.shiftwidth = 2
+         end
+		end,
 	},
 })
