c6936a31d2710f0a4412972d91eb762d1018d0a4
TheCrabilia <69972734+TheCrabilia@users.noreply.github.com>
2023-09-30 14:44:41 +0300

    Migrate indent-blankline to v3

--- nvim/.config/nvim/lua/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -1,31 +1,44 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
 	event = { "BufReadPre", "BufNewFile" },
+	main = "ibl",
+	version = "3.*",
 	opts = {
-		char = "┆",
-		context_char = "│",
-		use_treesitter = true,
-		show_trailing_blankline_indent = false,
-		show_first_indent_level = false,
-		show_current_context = true,
-		show_current_context_start = false,
-		filetype_exclude = {
-			"checkhelth",
-			"dap-repl",
-			"dapui_breakpoints",
-			"dapui_console",
-			"dapui_scopes",
-			"dapui_stacks",
-			"dapui_watches",
-			"fugitive",
-			"gitcommit",
-			"help",
-			"lazy",
-			"lspinfo",
-			"man",
-			"mason",
-			"tsplayground",
-			"DressingInput",
+		indent = {
+			char = "┆",
+		},
+		scope = {
+			enabled = true,
+			context_char = "│",
+			show_start = false,
+			show_end = false,
+		},
+		exclude = {
+			filetypes = {
+				"checkhelth",
+				"dap-repl",
+				"dapui_breakpoints",
+				"dapui_console",
+				"dapui_scopes",
+				"dapui_stacks",
+				"dapui_watches",
+				"fugitive",
+				"gitcommit",
+				"help",
+				"lazy",
+				"lspinfo",
+				"man",
+				"mason",
+				"tsplayground",
+				"DressingInput",
+			},
 		},
 	},
+	config = function(_, opts)
+		require("ibl").setup(opts)
+
+		local hooks = require("ibl.hooks")
+		hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
+		hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+	end,
 }
