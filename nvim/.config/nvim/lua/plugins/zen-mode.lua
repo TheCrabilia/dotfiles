c75f44589dc0daad3f2843b02c3568cbb8991f38
TheCrabilia <69972734+TheCrabilia@users.noreply.github.com>
2023-10-08 13:01:23 +0300

    Add zen-mode plugin

---
+++ nvim/.config/nvim/lua/plugins/zen-mode.lua
@@ -0,0 +1,33 @@
+return {
+	"folke/zen-mode.nvim",
+	cmd = { "ZenMode" },
+	opts = {
+		window = {
+			width = 1,
+			height = 1,
+			options = {
+				signcolumn = "no",
+				number = false,
+				relativenumber = false,
+				cursorline = false,
+				cursorcolumn = false,
+				colorcolumn = "",
+				foldcolumn = "0",
+				list = false,
+			},
+		},
+		plugins = {
+			gitsigns = { enabled = true },
+			tmux = { enabled = true },
+			alacritty = {
+				enabled = true,
+				font = "18",
+			},
+			options = {
+				enabled = true,
+				showcmd = false,
+				laststatus = 0,
+			},
+		},
+	},
+}
