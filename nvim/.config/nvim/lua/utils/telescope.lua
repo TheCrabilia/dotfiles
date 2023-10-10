0df0c9544f2cde276939ae6a821af050c6b8fb8e
TheCrabilia <69972734+TheCrabilia@users.noreply.github.com>
2023-10-10 21:30:32 +0300

    Remove unused functions

--- nvim/.config/nvim/lua/utils/telescope.lua
+++ nvim/.config/nvim/lua/utils/telescope.lua
@@ -15,32 +15,12 @@ local find_files_theme = require("telescope.themes").get_dropdown({
 
 local M = {}
 
-function M.find_in_nvim_config()
-	local opts = {
-		prompt_title = "~ nvim dotfiles ~",
-		shorten_path = false,
-		cwd = "~/.config/nvim",
-		follow = true,
-	}
-	builtin.find_files(vim.tbl_extend("force", find_files_theme, opts))
-end
-
-function M.find_in_zsh_config()
-	local opts = {
-		prompt_title = "~ zsh dotfiles ~",
-		shorten_path = false,
-		cwd = "~/.config/zsh",
-		follow = true,
-	}
-	builtin.find_files(vim.tbl_extend("force", find_files_theme, opts))
-end
-
 function M.find_files()
 	local opts = {
 		hidden = true,
 		no_ignore = true,
 		file_ignore_patterns = {
-			".git/",
+			".git",
 
 			-- Node
 			"node_modules",
