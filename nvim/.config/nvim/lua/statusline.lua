38fad4a5af04c89eb983832a11663fe8f902f209
TheCrabilia <69972734+TheCrabilia@users.noreply.github.com>
2023-12-03 20:01:49 +0200

    add statusline for fugitive

--- nvim/.config/nvim/lua/statusline.lua
+++ nvim/.config/nvim/lua/statusline.lua
@@ -8,61 +8,74 @@ local default_statusline = function()
 		"%=",
 		" %{&fileencoding?&fileencoding:&encoding}",
 		" [%{&fileformat}]",
-		"%y",
+		" %y",
 		" %p%%",
-		" %l:%c",
+		" %l:%c ",
 	})
 end
 
 local qf_statusline = function()
 	return table.concat({
 		"%#StatusLineText#",
-		"[Quickfix List]",
+		" [Quickfix List]",
 		"%=",
 		" %{&fileencoding?&fileencoding:&encoding}",
 		" [%{&fileformat}]",
-		"%y",
+		" %y",
 		" %p%%",
-		" %l:%c",
+		" %l:%c ",
 	})
 end
 
 local mason_statusline = function()
 	return table.concat({
 		"%#StatusLineText#",
-		"[Mason]",
+		" [Mason]",
 		"%=",
 		" %{&fileencoding?&fileencoding:&encoding}",
 		" [%{&fileformat}]",
-		"%y",
+		" %y",
 		" %p%%",
-		" %l:%c",
+		" %l:%c ",
 	})
 end
 
 local lazy_statusline = function()
 	return table.concat({
 		"%#StatusLineText#",
-		"[Lazy]",
+		" [Lazy]",
 		"%=",
 		" %{&fileencoding?&fileencoding:&encoding}",
 		" [%{&fileformat}]",
-		"%y",
+		" %y",
 		" %p%%",
-		" %l:%c",
+		" %l:%c ",
 	})
 end
 
 local telescope_statusline = function()
 	return table.concat({
 		"%#StatusLineText#",
-		"[Telescope]",
+		" [Telescope]",
 		"%=",
 		" %{&fileencoding?&fileencoding:&encoding}",
 		" [%{&fileformat}]",
-		"%y",
+		" %y",
 		" %p%%",
-		" %l:%c",
+		" %l:%c ",
+	})
+end
+
+local fugitive_statusline = function()
+	return table.concat({
+		"%#StatusLineText#",
+		" [Fugitive]",
+		"%=",
+		" %{&fileencoding?&fileencoding:&encoding}",
+		" [%{&fileformat}]",
+		" %y",
+		" %p%%",
+		" %l:%c ",
 	})
 end
 
@@ -71,6 +84,7 @@ local status_lines = {
 	mason = mason_statusline,
 	lazy = lazy_statusline,
 	TelescopePrompt = telescope_statusline,
+	fugitive = fugitive_statusline,
 }
 
 local augroup = vim.api.nvim_create_augroup("StatusLine", { clear = true })
@@ -78,8 +92,8 @@ local augroup = vim.api.nvim_create_augroup("StatusLine", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
 	group = augroup,
 	callback = function(ev)
-		if status_lines[ev.file] ~= nil then
-			vim.opt_local.statusline = status_lines[ev.file]()
+		if status_lines[ev.match] ~= nil then
+			vim.opt_local.statusline = status_lines[ev.match]()
 		else
 			vim.opt_local.statusline = default_statusline()
 		end
