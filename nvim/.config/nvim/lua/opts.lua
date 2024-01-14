be257dd5ab45ca608f26cdefaffa60db4e0ed137
TheCrabilia <69972734+TheCrabilia@users.noreply.github.com>
2024-01-14 15:06:16 +0200

    tweak some netrw options

--- nvim/.config/nvim/lua/opts.lua
+++ nvim/.config/nvim/lua/opts.lua
@@ -1,9 +1,10 @@
 vim.g.mapleader = " "
-vim.g.maplocalleader = " "
 
 -- Netrw
+vim.g.netrw_keepdir = 0
 vim.g.netrw_altfile = 1
 vim.g.netrw_banner = 0
+vim.g.netrw_localcopydircmd = "cp -r"
 
 -- Misc
 vim.opt.autowrite = true
