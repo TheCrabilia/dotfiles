b4df994a7bee5ef1a6d02383f5d77a18234bb4f1
TheCrabilia <69972734+TheCrabilia@users.noreply.github.com>
2024-03-03 15:10:48 +0200

    configure proxy

--- nvim/.config/nvim/lua/plugins/chat-gpt.lua
+++ nvim/.config/nvim/lua/plugins/chat-gpt.lua
@@ -14,5 +14,13 @@ return {
 	},
 	opts = {
 		api_key_cmd = "op read op://Personal/OpenAI/credential --no-newline",
+		extra_curl_params = {
+			"--proxy",
+			"http://127.0.0.1:3128",
+		},
+		openai_params = {
+			model = "gpt-4-1106-preview",
+			max_tokens = 500,
+		},
 	},
 }
