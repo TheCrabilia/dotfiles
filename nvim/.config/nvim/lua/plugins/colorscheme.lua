61b290f9d19aba66348e265a4a5e4c2c12c2477f
TheCrabilia <69972734+TheCrabilia@users.noreply.github.com>
2024-03-09 16:51:17 +0200

    define colors for gitsigns in colorscheme config

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,6 @@
 return {
 	"rose-pine/neovim",
 	name = "rose-pine",
-	enabled = true,
 	version = "v1.*",
 	lazy = false,
 	priority = 1000,
@@ -11,9 +10,14 @@ return {
 		disable_italics = true,
 		highlight_groups = {
 			ColorColumn = { bg = "text", blend = 10 },
-			CursorLine = { bg = "text", blend = 10 },
+			CursorLine = { bg = "text", blend = 7 },
 			TelescopeSelection = { bg = "text", blend = 10 },
 			PmenuSel = { bg = "text", blend = 10 },
+			StatusLineNC = { bg = "text", fg = "NONE", blend = 10 },
+			-- Disable background for GitSigns
+			GitSignsAdd = { fg = "#9ccfd8", bg = "none" },
+			GitSignsChange = { fg = "#ebbcba", bg = "none" },
+			GitSignsDelete = { fg = "#eb6f92", bg = "none" },
 		},
 	},
 	config = function(_, opts)
