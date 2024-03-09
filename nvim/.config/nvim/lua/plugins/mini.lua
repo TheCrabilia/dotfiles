b5e3bc25a15db53309a564706dbd54f28a141d7f
TheCrabilia <69972734+TheCrabilia@users.noreply.github.com>
2024-03-09 13:07:43 +0200

    add mini.ai and mini.surround

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,6 +1,21 @@
 return {
 	"echasnovski/mini.nvim",
 	config = function()
+		local ai = require("mini.ai")
+		local spec_treesitter = ai.gen_spec.treesitter
+		require("mini.ai").setup({
+			n_lines = 500,
+			custom_textobjects = {
+				f = spec_treesitter({ a = "@function.outer", i = "@function.inner" }),
+				c = spec_treesitter({
+					a = { "@conditional.outer", "@loop.outer" },
+					i = { "@conditional.inner", "@loop.inner" },
+				}),
+			},
+		})
+
+		require("mini.surround").setup()
+
 		local statusline = require("mini.statusline")
 		statusline.setup({
 			use_icons = true,
