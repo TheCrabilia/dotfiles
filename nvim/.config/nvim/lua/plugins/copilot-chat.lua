e6813d90cd81bd59ab6f2b80f7d4dcf39e92b284
TheCrabilia <69972734+TheCrabilia@users.noreply.github.com>
2024-03-28 20:25:49 +0200

    add copilot-chat

---
+++ nvim/.config/nvim/lua/plugins/copilot-chat.lua
@@ -0,0 +1,19 @@
+return {
+	"CopilotC-Nvim/CopilotChat.nvim",
+	cmd = {
+		"CopilotChat",
+		"CopilotChatCommit",
+		"CopilotChatCommitStaged",
+		"CopilotChatDocs",
+		"CopilotChatExplain",
+		"CopilotChatFix",
+		"CopilotChatFixDiagnostic",
+		"CopilotChatOptimize",
+		"CopilotChatTests",
+	},
+	dependencies = {
+		"zbirenbaum/copilot.lua",
+		"nvim-lua/plenary.nvim",
+	},
+	opts = {},
+}
