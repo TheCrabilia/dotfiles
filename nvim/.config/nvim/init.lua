f4be0e1d0e3032c3b18cb5729499f11dbb48bcc9
TheCrabilia <69972734+TheCrabilia@users.noreply.github.com>
2024-05-16 21:03:38 +0300

    user nvim plugin directory

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,48 @@
-require("core.options")
-require("core.bootstrap")
-require("core.globals")
-require("core.filetypes")
-require("core.autocmd")
-require("core.keymaps")
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({ import = "plugins" }, {
+	install = {
+		missing = true,
+		colorscheme = { "rose-pine" },
+	},
+	checker = {
+		enabled = true,
+		concurrency = nil,
+		notify = false,
+		frequency = 3600,
+	},
+	change_detection = {
+		enabled = true,
+		notify = false,
+	},
+	ui = { border = "rounded" },
+	performance = {
+		cache = {
+			enabled = true,
+		},
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
+})
